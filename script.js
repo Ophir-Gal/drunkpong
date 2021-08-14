ddd02fb933b345ef485f62d6c5cdce0ca5a4be02
ophir <ophirgal2@gmail.com>
2021-08-14 11:21:02 -0400

    GUI code mostly ready for plugging in the game logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const d_select = document.querySelector.bind(document)
+const d_select_id = document.getElementById.bind(document)
 
 // Set up Constants & Variables
 const LEFT_KEY = 37;
@@ -7,13 +8,57 @@ const RIGHT_KEY = 39;
 const DOWN_KEY = 40;
 const ESC_KEY = 27;
 
-function keyPress(event) {
+var playerState = 'NOTHING'
+var timerID = null;
+let clientWidth = window.innerWidth || document.body.clientWidth
+let clientHeight = window.innerHeight || document.body.clientHeight
+let leftPaddle = d_select_id('left-paddle')
+let rightPaddle = d_select_id('right-paddle')
+let ball = d_select_id('ball')
+
+var gameState = {cpuPaddle: {x: 0.1, y: 0.3}, userPaddle: {x: 0.1, y: 0.3}} // TODO: DELETE THIS DUMMY GAME STATE (TO BE IMPLEMENTED BY CLAIRE)
+function resetGameState() {  // TODO: delete this (TO BE IMPLEMENTED BY CLAIRE)
+    gameState.cpuPaddle.y = 0.3
+    gameState.userPaddle.y = 0.3
+}
+function nextFrame() {       // TODO: delete this (TO BE IMPLEMENTED BY CLAIRE)
+    gameState.cpuPaddle.y += 0.003
+    gameState.userPaddle.y += 0.003
+}
+
+function keyDown(event) {
     switch (event.which) {
         case UP_KEY:
-            $('#right-paddle').css('top', $('#right-paddle').position().top - 20)
+            playerState = 'UP'
             break;
         case DOWN_KEY:
-            $('#right-paddle').css('top', $('#right-paddle').position().top + 20)
+            playerState = 'DOWN'
             break;
     }
 }
+
+function keyUp(event) {
+    playerState = 'NOTHING'
+}
+
+function mainLoop() {
+    // advance game state to the next frame
+    nextFrame(playerState)
+    //
+    if (false) { // TODO: add flag that indicates whether game should stop/end
+        clearInterval(timerID)
+    }
+    // update GUI based on updated game state
+    leftPaddle.setAttribute('y', (1 - gameState.cpuPaddle.y) * clientHeight + '%')
+    rightPaddle.setAttribute('y', (1 - gameState.userPaddle.y) * clientHeight + '%')
+    ball.setAttribute('x', gameState.ball.x * clientWidth + '%')
+    ball.setAttribute('y', (1 - gameState.ball.y) * clientHeight + '%')
+}
+
+function startNewGame() {
+    resetGameState()
+    // start game's main loop
+    timerID = setInterval(mainLoop, 33)
+}
+
+startNewGame()
